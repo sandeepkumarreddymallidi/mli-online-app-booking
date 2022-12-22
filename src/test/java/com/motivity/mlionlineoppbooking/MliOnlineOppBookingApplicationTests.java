abd5a2e7a34526328ee6618a4dece9d7ff8add9a
sandeepkumarreddymallidi <106801934+sandeepkumarreddymallidi@users.noreply.github.com>
2022-12-22 15:44:57 +0530

    test

---
+++ src/test/java/com/motivity/mlionlineoppbooking/MliOnlineOppBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motivity.mlionlineoppbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MliOnlineOppBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
